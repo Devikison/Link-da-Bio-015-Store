1dbe36a25295b3f0ad65e5737406c8638da569c6
Devikison <devikisonads@gmail.com>
2026-09-17 11:55:19 -0300

    Add redesigned link-in-bio page for 015 Store

---
+++ serve.ps1
@@ -0,0 +1,40 @@
+$root = $PSScriptRoot
+$listener = New-Object System.Net.HttpListener
+$listener.Prefixes.Add("http://localhost:5173/")
+$listener.Start()
+Write-Output "Serving $root on http://localhost:5173/"
+
+$mime = @{
+  ".html"="text/html"; ".htm"="text/html"; ".css"="text/css"; ".js"="application/javascript";
+  ".png"="image/png"; ".jpg"="image/jpeg"; ".jpeg"="image/jpeg"; ".svg"="image/svg+xml";
+  ".ico"="image/x-icon"; ".json"="application/json"
+}
+
+while ($listener.IsListening) {
+  $ctx = $listener.GetContext()
+  $req = $ctx.Request
+  $res = $ctx.Response
+  try {
+    $path = $req.Url.LocalPath
+    if ($path -eq "/") { $path = "/index.html" }
+    $filePath = Join-Path $root ($path.TrimStart('/'))
+    $filePath = [System.Uri]::UnescapeDataString($filePath)
+    if (Test-Path $filePath -PathType Leaf) {
+      $ext = [System.IO.Path]::GetExtension($filePath).ToLower()
+      $ct = $mime[$ext]
+      if (-not $ct) { $ct = "application/octet-stream" }
+      $bytes = [System.IO.File]::ReadAllBytes($filePath)
+      $res.ContentType = $ct
+      $res.ContentLength64 = $bytes.Length
+      $res.OutputStream.Write($bytes, 0, $bytes.Length)
+    } else {
+      $res.StatusCode = 404
+      $msg = [System.Text.Encoding]::UTF8.GetBytes("404 Not Found: $path")
+      $res.OutputStream.Write($msg, 0, $msg.Length)
+    }
+  } catch {
+    $res.StatusCode = 500
+  } finally {
+    $res.OutputStream.Close()
+  }
+}
